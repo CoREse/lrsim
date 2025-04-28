4f69310cbef1ae33cfbda645aa1e6b15edcb9add
CRE <cre@hnas-lxc.lan>
2025-04-28 16:01:00 +0800

    v0.7: new model file, new mechanisms. (Thesis version)

--- extractModel.py
+++ extractModel.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import sys
+import struct
 
-Version=0
+Version=1
 
 Lengths=[]
 IDs=[]#(length, ins count, del count)
@@ -30,15 +31,23 @@ if len(sys.argv)==1 or sys.argv[1]=="-":
     f.close()
 Ins=0
 Del=0
-for ID in IDs[:3]:
+for ID in IDs[:1]:
     Ins+=ID[0]*ID[1]
     Del+=ID[0]*ID[2]
 Del/=Ins/100
 Ins=100
 Del=int(Del+0.5)
+Linkage=1
+if len(IDs)>1:
+    Linkage=0
+    for i in range(1,len(IDs[:4])):
+        Linkage+=IDs[i][1]/IDs[i][2]/(IDs[i-1][1]/IDs[i-1][2])
+    Linkage/=len(IDs[:4])-1
+Linkage=float(Linkage)
 sys.stdout.buffer.write(Version.to_bytes(4,sys.byteorder))
 sys.stdout.buffer.write(Ins.to_bytes(4,sys.byteorder))
 sys.stdout.buffer.write(Del.to_bytes(4,sys.byteorder))
+sys.stdout.buffer.write(struct.pack(f'{">" if sys.byteorder=="big" else "<"}d',Linkage))
 sys.stdout.buffer.write(len(Lengths).to_bytes(4,sys.byteorder))
 for Length, Former in Lengths:
     sys.stdout.buffer.write(Length.to_bytes(4,sys.byteorder))
